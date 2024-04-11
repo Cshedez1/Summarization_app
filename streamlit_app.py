380345dcd94e89e9f7f9d03a0f478caa31a5877d
Cshedez1 <161688060+Cshedez1@users.noreply.github.com>
2024-04-11 14:12:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ def generate_response(txt):
 
 
 # Page title
-st.set_page_config(page_title='🦜🦜 Text Summarization App')
-st.title('🦜🦜 Text Summarization App')
+st.set_page_config(page_title='🔗 Text Summarization App🔗')
+st.title('🔗Text Summarization App🔗')
 
 
 # Text input
